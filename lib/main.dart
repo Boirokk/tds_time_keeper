e5f1300f0b0c84526e0b3e76ec0c2e866896e287
Boirokk <chadzx10@gmail.com>
2023-01-18 22:43:22 -0500

    Initialie Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import './homepage.dart';
 
 main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+   initializeFirebase() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+      super.initState();
+      initializeFirebase();
+  }
 
   @override
   Widget build(BuildContext context) {
